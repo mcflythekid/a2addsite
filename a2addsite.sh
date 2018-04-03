37f2489bc9ca7571074bd9f8467227c581b72578
McFly the Kid <odopoc@gmail.com>
2018-04-04 00:48:44 +0700

    Update a2addsite.sh

--- a2addsite.sh
+++ a2addsite.sh
@@ -14,8 +14,9 @@ if [ "$(whoami)" != 'root' ]; then
 fi
 if [ "$action" != "add" ] && [ "$action" != "del" ]; then
 	echo $"Usage:
-	sudo a2addsite add domain_name [\"alias_name1 alias_name2\"]
-	sudo a2addsite del domain_name
+	\$ sudo a2addsite add domain_name [alias_name]
+	\$ sudo a2addsite add domain_name [\"alias_name1 alias_name2\"]
+	\$ sudo a2addsite del domain_name
 	"
     exit 0;
 fi
